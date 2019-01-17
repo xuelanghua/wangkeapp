8e4f94ce694b09e6d7c2578639d360af138de407
xuelang <1045578710@qq.com>
2019-01-17 16:26:52 +0800

    201901171626

--- js/util.js
+++ js/util.js
@@ -2,3 +2,10 @@ $ajaxUrl = 'https://wangke.wxschool.net/app/index.php?i=2&c=entry&m=longbing_car
 var getUserInfo = function() {
 	return JSON.parse(localStorage.getItem('user'));
 }
+
+function updateUserInfo(userInfo) {
+	mui.post($ajaxUrl + 'member&action=info', {token: userInfo.token}, function(res) {
+		localStorage.setItem('user', JSON.stringify(res.data));
+		console.log(JSON.stringify(res));
+	})
+}
