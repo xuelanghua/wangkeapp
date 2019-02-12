f8be65c1f4e20e81cfa18dbd2a541f7eb6ca17c1
xuelang <1045578710@qq.com>
2019-02-12 11:16:03 +0800

    201902121115

--- js/util.js
+++ js/util.js
@@ -1,4 +1,4 @@
-$ajaxUrl = 'https://wangke.wxschool.net/app/index.php?i=2&c=entry&m=longbing_card&do=';
+$ajaxUrl = 'https://wangke.ynhost.cn/app/index.php?i=2&c=entry&m=longbing_card&do=';
 var getUserInfo = function() {
 	return JSON.parse(localStorage.getItem('user'));
 }
