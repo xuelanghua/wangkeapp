1cc8fd79287643a0d398581ad09bbfff4b53da96
xuelang <1045578710@qq.com>
2019-11-21 16:13:27 +0800

    201911211613

--- js/util.js
+++ js/util.js
@@ -1,12 +1,12 @@
-$ajaxUrl = 'https://app.wangkeapp.cn/app/index.php?i=6&c=entry&m=longbing_card&do='; //服务器数据请求接口
-$uploadUrl = 'https://app.wangkeapp.cn/app/index.php?i=6&c=utility&a=file&do=upload&type=image'; //图片上传接口
-$videoUrl = 'https://app.wangkeapp.cn//app/index.php?i=6&c=utility&a=file&do=upload&type=video'; //视频上传接口
-$voiceUrl = 'https://app.wangkeapp.cn/app/index.php?i=6&c=entry&m=longbing_card&do=upload'; //音频上传接口
+// $ajaxUrl = 'https://app.wangkeapp.cn/app/index.php?i=6&c=entry&m=longbing_card&do='; //服务器数据请求接口
+// $uploadUrl = 'https://app.wangkeapp.cn/app/index.php?i=6&c=utility&a=file&do=upload&type=image'; //图片上传接口
+// $videoUrl = 'https://app.wangkeapp.cn//app/index.php?i=6&c=utility&a=file&do=upload&type=video'; //视频上传接口
+// $voiceUrl = 'https://app.wangkeapp.cn/app/index.php?i=6&c=entry&m=longbing_card&do=upload'; //音频上传接口
 
-// $ajaxUrl = 'http://app.ynhost.cn/app/index.php?i=6&c=entry&m=wangke&do='; //服务器数据请求接口
-// $uploadUrl = 'http://app.ynhost.cn/app/index.php?i=6&c=utility&a=file&do=upload&type=image'; //图片上传接口
-// $videoUrl = 'http://app.ynhost.cn/app/index.php?i=6&c=utility&a=file&do=upload&type=video'; //视频上传接口
-// $voiceUrl = 'http://app.ynhost.cn/app/index.php?i=6&c=entry&m=wangke&do=upload'; //音频上传接口
+$ajaxUrl = 'http://app.ynhost.cn/app/index.php?i=6&c=entry&m=wangke&do='; //服务器数据请求接口
+$uploadUrl = 'http://app.ynhost.cn/app/index.php?i=6&c=utility&a=file&do=upload&type=image'; //图片上传接口
+$videoUrl = 'http://app.ynhost.cn/app/index.php?i=6&c=utility&a=file&do=upload&type=video'; //视频上传接口
+$voiceUrl = 'http://app.ynhost.cn/app/index.php?i=6&c=entry&m=wangke&do=upload'; //音频上传接口
 
 $debug = true;
 $videoMaxSize = 10 * 1024 * 1024;
